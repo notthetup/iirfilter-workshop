49f4188eaa2b4b050f4c9387b14cae646a2f4c49
Chinmay Pendharkar <notthetup@gmail.com>
2016-06-14 16:02:41 +0800

    basic working version

--- script.js
+++ script.js
@@ -1,30 +1,149 @@
-var horner = require("horner");
-var ndarray = require("ndarray");
 var cops = require("ndarray-complex");
+var freqz = require('ndarray-freqz');
+var zeros = require("zeros");
+var Chart = require('chart.js');
 
-console.log("Hello World");
 
-/*
-* Given the numerator b and denominator a of a digital filter, compute its frequency response
-*/
-function caclculateFreqz(a, b){
+// Sample Data
+var b = [ 0.00685858,  0.00424427,  0.01363637,  0.00939893,  0.01363637, 0.00424427,  0.00685858]
+var a = [ 1 , -3.6133816 ,  6.29949582, -6.44744259,  4.04658649, -1.4649745 ,  0.23927553]
 
+window.addEventListener('load', function(){
+
+  calculateAndPlot(b,a);
+  var button = document.getElementById('plotbutton');
+  var inputA = document.getElementById('inputA');
+  var inputB = document.getElementById('inputB');
+  var error = document.getElementById('error');
+
+  button.addEventListener('click', function(){
+    error.hidden = true;
+    var aStr = inputA.value;
+    var bStr = inputB.value;
+    var a = parseArrayString(aStr);
+    var b = parseArrayString(bStr);
+    calculateAndPlot(b,a);
+  });
+
+});
+
+function parseArrayString(str){
+  var array = [];
+  str = str.trim();
+  if (str[0] == '[' && str[str.length-1] == ']'){
+    try{
+      array = JSON.parse(str);
+    }catch(e){
+      error.hidden = false;
+    }
+  }else{
+    try{
+      array = JSON.parse('[' + str + ']');
+    }catch(e){
+      error.hidden = false;
+    }
+  }
+  return array;
+}
+
+function calculateAndPlot(b,a){
+  var response = calculateResponse(b,a);
+  plotResponse({
+    id:'magChart',
+    data: response.magData,
+    title: "Magnitude Response",
+    yLable: 'Magnitude (dB)',
+    xLable: 'Frequency (Hz)'
+  });
+
+  plotResponse({
+    id:'phaseChart',
+    data: response.phaseDate,
+    title: "Phase Response",
+    yLable: 'Phase (rad)',
+    xLable: 'Frequency (Hz)'
+  });
+}
+
+function calculateResponse(b, a){
+  var fr = freqz(b, a);
+
+  var magnitude = zeros([fr.H_i.data.length])
+  var phase = zeros([fr.H_i.data.length])
+
+  cops.abs(magnitude, fr.H_r, fr.H_i);
+  cops.arg(phase, fr.H_r, fr.H_i);
+
+  var magData = [];
+  magnitude.data.forEach(function(thisMag, index){
+    magData.push({
+      'x': fr.omega.data[index]/Math.PI*44100,
+      'y': 20*Math.log10(thisMag)
+    });
+  });
+
+  var phaseData = [];
+  phase.data.forEach(function(thisPhase, index){
+    phaseData.push({
+      'x': fr.omega.data[index]/Math.PI*44100,
+      'y': 20*Math.log10(thisPhase)
+    });
+  });
 
   return {
-    w: [],
-    H: []
+    'magData': magData,
+    'phaseDate': phaseData
   }
 }
 
-var b = [2, 0, 4];
-var a = [0, -3, 5];
+function plotResponse(options){
 
-var omega = ndarray(new Float32Array([10,20,30,40,50]), [1, 5])
-console.log(omega);
-window.omega = omega;
-
-//Evaluate the polynomial 2 - 3i * x + (4 + 5i)*x^2 at x = 1+2i:
-
-// console.log(horner([]     //Real coefficients
-//                   ,[]   //Imaginary coefficients
-//                   ,[1, 2]))
+  var magCtx = document.getElementById(options.id);
+  var myChart = new Chart(magCtx, {
+    type: 'line',
+    data: {
+      labels: [],
+      datasets: [
+        {
+          fill: false,
+          borderColor: "rgba(75,192,192,1)",
+          pointRadius: 1,
+          lineTension: 0.15,
+          data: options.data,
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      legend: {
+        display: false
+      },
+      title: {
+        display: true,
+        text: options.title
+      },
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: options.yLable
+          }
+        }],
+        xAxes: [{
+           type: 'linear',
+           position: 'bottom',
+           scaleLabel: {
+             display: true,
+             labelString: options.xLable
+           },
+           ticks: {
+               min: 10,
+               max: 44100
+           }
+       }]
+      },
+      responsive: false,
+      maintainAspectRatio: true
+    }
+  });
+}
